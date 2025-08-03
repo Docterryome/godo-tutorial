5cfa9b38786951a6b950b6db519afea0755eba7a
Docterryome Wilkins <docterryomewilkins@Docterryomes-MacBook-Pro.local>
2025-08-02 22:24:16 -0500

    added moving platforms

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const MAX_JUMP = 1
+const JUMP_DELTA = .9
 @onready var anmated_sprite = $AnimatedSprite2D
+var jump_num = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,7 +15,12 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		jump_num = 1
 		velocity.y = JUMP_VELOCITY
+	# Double Jump
+	elif Input.is_action_just_pressed("ui_accept") and jump_num <= MAX_JUMP:
+		jump_num += 1
+		velocity.y = JUMP_VELOCITY * JUMP_DELTA
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,7 +33,9 @@ func _physics_process(delta: float) -> void:
 	
 	if direction:
 		velocity.x = direction * SPEED
+		anmated_sprite.animation = "run"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anmated_sprite.animation = "idle"
 
 	move_and_slide()
